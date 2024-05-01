8b47d9ed71b58359f5156df2c8310ac019e2767f
Amin Genesis <117842597+amingenesis@users.noreply.github.com>
2024-05-01 18:50:23 +0000

    new changes in main

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+    
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 10)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
